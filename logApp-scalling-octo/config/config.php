3cf7a42daa4abc5734aa047bd48b94adcdc8ad40
mark3475 <rolansantosbete60@gmail.com>
2021-06-01 17:41:20 +0800

    config.php

--- logApp-scalling-octo/config/config.php
+++ logApp-scalling-octo/config/config.php
@@ -1,6 +1,6 @@
 <?php
-	define('ROOT_URL', '');
-	define('DB_HOST', '');
-	define('DB_USER', '');
-	define('DB_PASS', '');
-	define('DB_NAME', '');
+	define('ROOT_URL', 'guestbook-login.php');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6416305');
+	define('DB_PASS', 'lujs9FqaiT');
+	define('DB_NAME', 'sql6416305');
